83dc4ac25900dc087e0e16b53a1af71d58d4428e
Raul <raul.alhena@gmail.com>
2024-02-07 01:35:53 +0100

    client & server working propperly & basic pipeline created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [ data, setData ] = useState('Not connecting to server');
+
+  const getData = async () => {
+    const resp = await fetch('http://localhost:3000/');
+    const result = await resp.json();
+
+    setData(result);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
       <div className='text'>
-        <h1>Pipeline is working in VPS!</h1>
+        <h1>{data.message}</h1>
       </div>
     </>
   );
